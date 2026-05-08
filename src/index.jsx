12e65059ab6bbe5afacf6f2f996e90a2268cf47d
Andy Palmero <4ndyraul@gmail.com>
2026-05-08 00:18:35 -0400

    style: polish quick compose popup

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { MyForm } from './components/Form'
+import './styles.css'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
